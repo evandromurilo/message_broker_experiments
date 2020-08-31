d9db0caff69cec0893123a9b33f1d011e8a1a12a
Evandro Murilo <evandromurilob@hotmail.com>
2020-08-31 14:54:44 -0400

    first commit, tutorials 1 and 2 from rabbitmq.com

---
+++ rabbitmq/new_task.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import pika, sys
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=message)
+print(" [x] Sent '%r'" % message)
+
+connection.close()
